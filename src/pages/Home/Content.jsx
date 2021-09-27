562b20ea0315269b92a010d38153d6222f45279f
Rangga Putra Pertama <ranggaputrapertamapp@gmail.com>
2021-09-27 21:52:25 +0700

    feat: fetch data from user service to content

--- src/pages/Home/Content.jsx
+++ src/pages/Home/Content.jsx
@@ -1,8 +1,26 @@
 import Card from "../../components/Card";
 import SearchBox from "../../components/SearchBox";
 import Title from "./Title";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Content() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://final-project-user-profile.herokuapp.com/user", {
+        method: "GET",
+        headers: {
+          access_token:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZnVsbF9uYW1lIjoiVmlraSBZYXB1dHJhIiwiZW1haWwiOiJ2aWtpLnlhcHV0cmFAZ21haWwuY29tIiwiaXNfcHJlbWl1bSI6dHJ1ZSwiaWF0IjoxNjMyNTg1NTE0fQ.yjNe-7cOf98KNwHW9sHUg0IThu8BalLHauF-vI4B_M0",
+        },
+      })
+      .then((results) => {
+        setUsers(results.data);
+      });
+  }, []);
+
   return (
     <div>
       <div className="flex justify-between p-7">
@@ -11,10 +29,18 @@ function Content() {
       </div>
       <hr className="mx-7 my-2" />
       <div className="grid grid-cols-2 gap-5 p-6">
-        <Card role="Coder" />
-        <Card role="Designer" />
-        <Card />
-        <Card role="Designer" />
+        {users.map((user) => {
+          return (
+            <Card
+              key={user.id}
+              full_name={user.full_name}
+              main_card_showoff={user.main_card_showoff}
+              portfolio_link={user.portfolio_link}
+              role={user.register_as}
+              social_media_link={user.social_media_link}
+            />
+          );
+        })}
       </div>
     </div>
   );
