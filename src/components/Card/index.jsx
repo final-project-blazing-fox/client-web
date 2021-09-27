726f9c16341a37dbb966b7a7c359c958ef740066
Rangga Putra Pertama <ranggaputrapertamapp@gmail.com>
2021-09-27 22:24:40 +0700

    feat: add main card showoff video or img

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,23 +1,33 @@
 import { FaHeart } from "react-icons/fa";
 
-function Card({ role, color = "text-gray-400" }) {
+function Card({
+  role,
+  color = "text-gray-400",
+  full_name,
+  main_card_showoff,
+  portfolio_link,
+  social_media_link,
+}) {
   return (
     <div className="w-full">
       <div className="bg-light-gray rounded-lg">
-        <div
-          className="bg-origin-border aspect-w-4 aspect-h-3 w-full bg-contain bg-no-repeat bg-center"
-          style={{
-            backgroundImage: "url(https://i.stack.imgur.com/AZUmQ.jpg)",
-          }}
-        ></div>
+        <div className="bg-origin-border aspect-w-4 aspect-h-3 w-full bg-contain bg-no-repeat bg-center">
+          {role === "coder" ? (
+            <video controls="controls" src={main_card_showoff} />
+          ) : (
+            <img src={main_card_showoff} alt="main card showoff" />
+          )}
+        </div>
       </div>
       <div
-        class="grid grid-cols-2 gap-2"
+        className="grid grid-cols-2 gap-2"
         style={{ gridTemplateColumns: "80% 1fr" }}
       >
         <div className="border-2 border-custom-green bg-white rounded-lg p-2 mt-2 text-center cursor-pointer hover:bg-gray-300">
-          <p className="text-2xl text-custom-gray">Budi</p>
+          <p className="text-2xl text-custom-gray">{full_name}</p>
           <p className="text-light-gray">{role}</p>
+          <p className="text-light-gray">{portfolio_link}</p>
+          <p className="text-light-gray">{social_media_link}</p>
         </div>
         <div
           className={`border-2 border-pink-300 cursor-pointer hover:bg-pink-300 ${color} rounded-md flex justify-center items-center p-4 mt-2`}
