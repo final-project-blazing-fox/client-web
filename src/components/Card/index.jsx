0c4109ae4e54448a89eb167b146b1889fcec538d
Ari Juanda <juandaari70@gmail.com>
2021-09-27 19:28:23 +0700

    style: add fav button

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,4 +1,6 @@
-function Card({ role }) {
+import { FaHeart } from "react-icons/fa";
+
+function Card({ role, color = "text-gray-400" }) {
   return (
     <div className="w-full">
       <div className="bg-light-gray rounded-lg">
@@ -9,9 +11,19 @@ function Card({ role }) {
           }}
         ></div>
       </div>
-      <div className="border-2 border-custom-green bg-white rounded-lg p-2 mt-2 text-center cursor-pointer hover:bg-gray-300">
-        <p className="text-2xl text-custom-gray">Budi</p>
-        <p className="text-light-gray">{role}</p>
+      <div
+        class="grid grid-cols-2 gap-2"
+        style={{ gridTemplateColumns: "80% 1fr" }}
+      >
+        <div className="border-2 border-custom-green bg-white rounded-lg p-2 mt-2 text-center cursor-pointer hover:bg-gray-300">
+          <p className="text-2xl text-custom-gray">Budi</p>
+          <p className="text-light-gray">{role}</p>
+        </div>
+        <div
+          className={`border-2 border-pink-300 cursor-pointer hover:bg-pink-300 ${color} rounded-md flex justify-center items-center p-4 mt-2`}
+        >
+          <FaHeart size={30} />
+        </div>
       </div>
     </div>
   );
