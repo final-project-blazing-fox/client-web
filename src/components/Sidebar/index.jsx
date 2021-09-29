deb99d009b1a66743ca88cbd873cf05ad4eb85db
Arip Budiman <aripbudiman007@gmail.com>
2021-09-29 17:43:33 +0700

    feat: add mathces user chat list

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -5,13 +5,40 @@ import Title from "./Title";
 import CreatePostContainer from "./CreatePostContainer";
 import Menu from "./Menu";
 import { GiHamburgerMenu } from "react-icons/gi";
+import firebase from "firebase";
+import { useEffect } from "react";
 
 function Sidebar() {
+  const userdata = localStorage.firebase_user
+    ? JSON.parse(localStorage.firebase_user)
+    : 1;
+  const [onlineUser, setOnlineUser] = useState([]);
   const [showMenu, setShowMenu] = useState(false);
 
   const menuHandler = () => {
     setShowMenu(!showMenu);
   };
+
+  useEffect(() => {
+    const db = firebase.firestore();
+
+    db.collection("users").onSnapshot((querySnapshot) => {
+      const users = [];
+      querySnapshot.forEach(function (doc) {
+        if (doc.data().uid !== userdata.uid) {
+          users.push(doc.data());
+        }
+      });
+
+      setOnlineUser(users);
+    });
+  }, []);
+
+  const arrlocal = localStorage.matches ? localStorage.matches : 0;
+
+  const matchUser = onlineUser.filter((user) =>
+    arrlocal.includes(user.localID)
+  );
 
   return (
     <MainContainer>
@@ -29,19 +56,18 @@ function Sidebar() {
         } lg:flex flex-col items-start justify-start text-gray-300 mt-5 border-t-2 border-gray-300 w-full pt-5`}
       >
         <p className="text-xl text-gray-500">Chat</p>
-        {/* template */}
-        <div className="cursor-pointer mt-2 hover:text-gray-50">
-          <Link to="/chat">Rangga</Link>
-        </div>
-        <div className="cursor-pointer mt-2 hover:text-gray-50">
-          <Link to="/chat">Abdul</Link>
-        </div>
-        <div className="cursor-pointer mt-2 hover:text-gray-50">
-          <Link to="/chat">Arip</Link>
-        </div>
-        <div className="cursor-pointer mt-2 hover:text-gray-50">
-          <Link to="/chat">Viki</Link>
-        </div>
+        {onlineUser
+          ? matchUser.map((online, index) => {
+              return (
+                <div
+                  key={online.uid}
+                  className="cursor-pointer mt-2 hover:text-gray-50"
+                >
+                  <Link to={`/chat/${online.uid}`}>{online.fullName}</Link>
+                </div>
+              );
+            })
+          : null}
       </div>
     </MainContainer>
   );
